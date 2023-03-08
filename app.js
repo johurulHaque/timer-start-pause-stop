a8d7d5c123b88a39bb1c57d2b5b09a08722fb2fc
johurulhque <johurulhque.cse@gmail.com>
2023-03-08 13:58:59 +0600

    all done

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let pTag = document.getElementById('demo');
 let count  = 0;
+let timer;
 
 // start timer
 document.getElementById('start').addEventListener('click',function(){
-    var intervalId = setInterval(()=>{
+    let intervalId = setInterval(()=>{
         count++;
         pTag.innerText = count;
     },1000);
+    timer = intervalId;
+});
+
+document.getElementById('pause').addEventListener('click',function(){
+    clearInterval(timer);
+});
+
+document.getElementById('reset').addEventListener('click',function(){
+    pTag.innerText = "";
+    count  = 0;
 });
 
 
